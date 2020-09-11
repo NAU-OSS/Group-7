7980184d6fd63f480a6aa5ba6f2a30de538fb884
Jared Weinberger <jared.j.weinberger@gmail.com>
2020-09-11 16:34:13 +0000

    removed redundant code

--- assignmnetPR/assignmnetPR.c
+++ assignmnetPR/assignmnetPR.c
@@ -1,31 +1,50 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+bool is_digit(char c)
+{
+    int intval = c - '0';
+    return intval < 10 && intval >= 0;
+}
+
+bool is_valid(char *numstring)
+{
+    char *curr_char = numstring;
+    while (*curr_char != '\0' && (is_digit(*curr_char) || *curr_char == '.' || *curr_char == '-'))
+    {
+        curr_char++;
+    }
+    return *curr_char == '\0';
+}
+
 bool characteristic(char numString[], int *c)
 {
     int index = 0;
     int signValue = 1;
     int cval = 0;
 
-    //TODO: validate numstring
-    
+    //validate numstring
+    if (!is_valid(numString))
+    {
+        return false;
+    }
+
     //find sign of numstring and find start index of number
     while (numString[index] != '\0' && numString[index] != '.')
     {
-        if(numString[index] == '-')
+        if (numString[index] == '-')
         {
             signValue = -1;
         }
-        
-        if( isdigit(numString[index]))
+
+        if (is_digit(numString[index]))
         {
             cval = (cval * 10) + (numString[index] - '0');
-                
         }
 
         index++;
     }
-    
+
     *c = cval * signValue;
 
     return true;
@@ -36,26 +55,30 @@ bool mantissa(char numString[], int *numerator, int *denominator)
     int index = 0;
     int dvalue = 1;
     int nvalue = 0;
-    
-    //TODO: validate numstring
+
+    //validate numstring
+    if (!is_valid(numString))
+    {
+        return false;
+    }
 
     //get to '.' or end of string
-    while(numString[index] != '\0')
+    while (numString[index] != '\0')
     {
-        if(numString[index] == '.')
+        if (numString[index] == '.')
         {
             index++;
 
-            while(isdigit(numString[index]))
+            while (is_digit(numString[index]))
             {
                 dvalue *= 10;
-                nvalue = (nvalue *10) + (numString[index] - '0');
+                nvalue = (nvalue * 10) + (numString[index] - '0');
 
                 index++;
             }
         }
 
-        index ++;
+        index++;
     }
 
     *numerator = nvalue;
@@ -64,21 +87,23 @@ bool mantissa(char numString[], int *numerator, int *denominator)
     return true;
 }
 
-
 int main()
 {
     int c = 0;
     int n = 0;
     int d = 0;
     char number[] = "   ----12.5987907  ";
-    
-    characteristic(number,  &c);
 
-    printf("%d\n", c);
-
-    mantissa(number, &n, &d);
-
-    printf("numerator: %d\n", n);
-    printf("denominator: %d\n", d);
-    return 0;
+    if (characteristic(number, &c) && mantissa(number, &n, &d))
+    {
+        printf("characteristic: %d\n", c);
+        printf("numerator: %d\n", n);
+        printf("denominator: %d\n", d);
+        return 0;
+    }
+    else
+    {
+        fprintf(stderr, "The string was invalid!\n\n");
+        return 42 + 27;
+    }
 }
